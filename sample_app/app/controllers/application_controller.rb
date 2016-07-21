f6c52f45840b67da187f0febaf3c1b6f4d0bd1e7
Nathan De Jong <nathanjdejong@gmail.com>
2016-07-21 10:11:36 -0500

    added custom actions

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+	# prevent csrf attacks by raising an exception.
+	# for APIs, you may want to use :null_session instead
   protect_from_forgery with: :exception
+
+  def hello
+  	render html: "hello, world!"
+  end
+
+  def test_action
+  	render html: "test_action"
+  end
 end
